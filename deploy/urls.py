ecea691bb1c42811b5e79b6fb3c39ebd25e06176
zladimir <zolodya@mail.ru>
2020-07-06 16:57:36 +0300

    third commit

--- deploy/urls.py
+++ deploy/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
+#from rest_framework.urlpatterns import format_suffix_patterns
 
 from fastbert import views
 
