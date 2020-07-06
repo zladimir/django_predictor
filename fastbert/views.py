ce5e6dd7d1ede3c5fb40f8764c83a88b90954e85
zladimir <zolodya@mail.ru>
2020-07-06 17:23:05 +0300

    third commit

--- fastbert/views.py
+++ fastbert/views.py
@@ -1,10 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 
-from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
 from .apps import WebappConfig
 from .forms import NameForm
 
