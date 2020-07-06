a6119fa41e58d5764353f5576ccbcc76d2f75f63
zladimir <zolodya@mail.ru>
2020-07-06 18:45:27 +0300

    third commit

--- fastbert/apps.py
+++ fastbert/apps.py
@@ -4,6 +4,8 @@ import pathlib
 import os
 import pickle
 from django.conf import settings
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 class WebappConfig(AppConfig):
     path = os.path.join(settings.MODELS, 'models.p')
